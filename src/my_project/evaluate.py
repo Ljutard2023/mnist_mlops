6dab5d26f652aca8231ec01f83ff7630ece601a3
Lucas <lucasjutard25@gmail.com>
2026-01-08 17:02:13 +0100

    Initial commit: MLOps pipeline structure setup

---
+++ src/my_project/evaluate.py
@@ -0,0 +1,32 @@
+import torch
+import typer
+from .data import corrupt_mnist
+from .model import MyAwesomeModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+def evaluate(model_checkpoint: str) -> None:
+    """Evaluate a trained model."""
+    print("Evaluating like my life depended on it")
+    print(model_checkpoint)
+
+    model = MyAwesomeModel().to(DEVICE)
+    # On charge les poids sauvegardés
+    model.load_state_dict(torch.load(model_checkpoint))
+
+    _, test_set = corrupt_mnist()
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=32)
+
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad(): # Bonne pratique: pas de calcul de gradient en éval
+        for img, target in test_dataloader:
+            img, target = img.to(DEVICE), target.to(DEVICE)
+            y_pred = model(img)
+            correct += (y_pred.argmax(dim=1) == target).float().sum().item()
+            total += target.size(0)
+    
+    print(f"Test accuracy: {correct / total:.2%}")
+
+if __name__ == "__main__":
+    typer.run(evaluate)
